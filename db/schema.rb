61e35c07fbb4fae7675a4ce7c9eba5edc0f23ca6
Katrina & James <katrinajames@appacademy.io>
2015-06-19 10:56:31 -0400

    Add session_token column to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150619143810) do
+ActiveRecord::Schema.define(version: 20150619145205) do
 
   create_table "users", force: true do |t|
     t.string   "email",           null: false
     t.string   "password_digest", null: false
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "session_token",   null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
