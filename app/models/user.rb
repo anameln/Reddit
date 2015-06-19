e3f05152d58aaf62b64f275e31104ef49050c4ff
Katrina & James <katrinajames@appacademy.io>
2015-06-19 10:51:44 -0400

    Build password methods for User class

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ActiveRecord::Base
+  validates :email, :password_digest, presence: true
+  validates :email, uniqueness: true
+  validates :password, length: { minimum: 3, allow_nil: true }
 end
