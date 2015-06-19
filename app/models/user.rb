57c0d95b09db35e50a650f0bf7677b4d9dd41da9
Katrina & James <katrinajames@appacademy.io>
2015-06-19 11:05:58 -0400

    implement session_token methods for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,37 @@
 class User < ActiveRecord::Base
-  validates :email, :password_digest, presence: true
+  validates :email, :password_digest, :session_token, presence: true
   validates :email, uniqueness: true
   validates :password, length: { minimum: 3, allow_nil: true }
+
+  after_initialize :ensure_session_token
+
+  attr_reader :password
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def self.find_by_credentials(email, password)
+    user = User.find_by(email: email)
+    return nil if user.nil? || !user.is_password?(password)
+    user
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(password_digest).is_password?(password)
+  end
+
+  def self.generate_session_token
+    SecureRandom.safeurl_base64(16)
+  end
+
+  def reset_session_token!
+    self.session_token = User.generate_session_token
+  end
+
+  def ensure_session_token
+    self.session_token ||= reset_session_token!
+  end
+
 end
