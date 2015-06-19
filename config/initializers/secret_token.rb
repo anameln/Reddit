9e24c9696631d0195a0d7bf7fb7c8285fb713f75
Katrina & James <katrinajames@appacademy.io>
2015-06-19 10:34:00 -0400

    initial commit. setup gemfile

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Reddit::Application.config.secret_key_base = 'd44d554ae13ed28675f144b3a30163a8dbbd5736e6363ba817d5f9cb255ee5cb621e52ed1772899782150f27aaa60d7795b4ddab4adec8dcd6e6650efd26b0f7'
